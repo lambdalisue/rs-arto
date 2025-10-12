a7ce3d1834d21fbb397ca9c7bbec871cf03a956c
Alisue <lambdalisue@gmail.com>
2025-10-12 10:31:09 +0900

    feat: Add theme selector feature on web side

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,1 +1,22 @@
-import "./main.css";
+import {
+  type ThemePreference,
+  type Theme,
+  setTheme,
+  getStoredPreference,
+  applyResolvedTheme,
+} from "./ts/theme-selector";
+
+// Extend the Window type
+declare global {
+  interface Window {
+    setMarkdownTheme: (theme: ThemePreference) => void;
+    getCurrentMarkdownTheme: () => ThemePreference;
+    applyMarkdownResolvedTheme: (theme: Theme) => void;
+  }
+}
+window.setMarkdownTheme = setTheme;
+window.getCurrentMarkdownTheme = getStoredPreference;
+window.applyMarkdownResolvedTheme = (theme: Theme) => {
+  const preference = getStoredPreference();
+  applyResolvedTheme(theme, preference);
+};
